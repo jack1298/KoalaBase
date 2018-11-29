c9fd44619ad86b05d50e78cb1f9e78ada39bfdee
jack1298 <jlim5225@gmail.com>
2018-11-29 11:46:57 +1100

    Update koalaBase_main.js
    updates to js. adding button logic. still require animation to be input.

--- koalaBase_main.js
+++ koalaBase_main.js
@@ -1,4 +1,5 @@
 //================DEFINE KOALAS=================== 
+
 //Kx = koala #, refer to excel file for full data
 
 var k1 = {
@@ -8,6 +9,7 @@ var k1 = {
 	found: "Tree",
 	fwc: "false", //found with children
 	dead: "false"
+	removed: "false"
 };
 
 var k2 = {
@@ -17,6 +19,7 @@ var k2 = {
 	found: "Tree",
 	fwc: "true", //found with children
 	dead: "false"
+	removed: "false"
 };
 
 var k3 = {
@@ -26,6 +29,7 @@ var k3 = {
 	found: "Tree",
 	fwc: "false", //found with children
 	dead: "false"
+	removed: "false"
 };
 
 
@@ -36,8 +40,63 @@ var k4 = {
 	found: "Tree",
 	fwc: "false", //found with children
 	dead: "false"
+	removed: "false"
 };
 
-//===================Logic========================
+var koalaArr = [k1, k2, k3, k4];
+
+//animation 
+//var koala = doc..... //select koala from html
+
+//Buttons
+var buttons = document.getElementsByTagName("button");
+
+//locations of button answers in the button array
+const Q1_M = 0;
+const Q1_F = 1;
+const Q2_A = 2;
+const Q2_B = 3;
+
+const finished = buttons.length - 1;
+
+//more questions to come... maybe
+
+//===================LOGIC========================
+//Question 1: 
+buttons[Q1_M].addEventListener("click", function()) {
+	/*
+		*Answer was male
+		*Move koala up the tree to the given location
+
+		*enable next question
+		*disable next question
+
+		*remove koalas from list
+	*/
+	alert("the male button was pressed");
+
+	for (var i = koalaArr.length - 1; i >= 0; i--) {
+		if (koalaArr[i].gender == "female") {
+			console.log(koalaArr[i].name + "removed");
+			koalaArr[i].removed = "true";
+
+		}
+	}
+}
+
+buttons[Q1_F].addEventListener("click", function()) {
+	alert the female button was pressed 
+}
+
+//Question 2: 
+
+//finish button pressed 
+buttons[finished].addEventListener("click", function()) {
+	/*
+		*call finished function
+		*check
+	*/
+}
+
 
 //===================FUNCTIONS=====================
